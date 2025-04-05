cb978664b12e6126161ab61cfea94a52194a9b46
Dilshod Turobov <deebov@yandex.com>
2025-04-05 20:39:19 +0200

    Update home screen

--- src/pages/home-screen.tsx
+++ src/pages/home-screen.tsx
@@ -1,102 +1,108 @@
-import { Fragment, lazy } from "react";
-import { ArrowRight } from "@untitledui/icons";
-import { BadgeGroup } from "@/components/shared/badges/badge-groups";
+import UntitledLogoMinimal from "@/components/foundations/logo/UntitledLogoMinimal";
 import { Button } from "@/components/shared/buttons/button";
-import { Form } from "@/components/shared/form/form";
-import { Input } from "@/components/shared/input";
+import ButtonUtility from "@/components/shared/buttons/button-utility";
+import { BookOpen01, Copy01, Cube01, HelpCircle } from "@untitledui/icons";
+import { lazy } from "react";
 
 const Header = lazy(() =>
-  import("@/components/marketing/header-navigation/components/header").then(
-    (mod) => ({ default: mod.Header }),
-  ),
+    import("@/components/marketing/header-navigation/components/header").then((mod) => ({ default: mod.Header }))
 );
+
 export const HomeScreen = () => {
-  return (
-    <Fragment>
-      <Header />
+    return (
+        <div className="flex h-dvh flex-col">
+            <Header />
 
-      <section className="relative bg-primary py-16 lg:flex lg:min-h-[720px] lg:items-center lg:py-24">
-        <div className="mx-auto flex w-full max-w-container items-center px-4 md:px-8">
-          <div className="flex flex-col items-start md:max-w-3xl lg:w-1/2 lg:pr-8">
-            <a
-              href="#"
-              className="rounded-[10px] outline-focus-ring focus:outline-2 focus:outline-offset-2"
-            >
-              <BadgeGroup
-                className="hidden md:flex"
-                size="lg"
-                addonText="We're hiring!"
-                iconTrailing={ArrowRight}
-                theme="modern"
-                color="brand"
-              >
-                Join our remote team
-              </BadgeGroup>
-              <BadgeGroup
-                className="md:hidden"
-                size="md"
-                addonText="We're hiring!"
-                iconTrailing={ArrowRight}
-                theme="modern"
-                color="brand"
-              >
-                Join our remote team
-              </BadgeGroup>
-            </a>
+            <div className="flex px-4 min-h-0 flex-1 flex-col items-center justify-center">
+                <div className="relative flex size-28 items-center justify-center">
+                    <UntitledLogoMinimal className="size-10" />
 
-            <h1 className="mt-4 text-display-md font-semibold text-primary md:text-display-lg lg:text-display-xl">
-              People who care about your growth
-            </h1>
-            <p className="mt-4 text-lg text-tertiary md:mt-6 md:max-w-lg md:text-xl">
-              Powerful, self-serve product and growth analytics to help you
-              convert, engage, and retain more.
-            </p>
+                    <svg
+                        width="480"
+                        height="480"
+                        viewBox="0 0 480 480"
+                        fill="none"
+                        className="absolute -z-1 text-border-secondary">
+                        <mask
+                            id="mask0_4933_393068"
+                            style={{ maskType: "alpha" }}
+                            maskUnits="userSpaceOnUse"
+                            x="0"
+                            y="0"
+                            width="480"
+                            height="480">
+                            <rect width="480" height="480" fill="url(#paint0_radial_4933_393068)" />
+                        </mask>
+                        <g mask="url(#mask0_4933_393068)">
+                            <circle cx="240" cy="240" r="47.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="79.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="111.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="143.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="143.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="175.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="207.5" stroke="currentColor" />
+                            <circle cx="240" cy="240" r="239.5" stroke="currentColor" />
+                        </g>
+                        <defs>
+                            <radialGradient
+                                id="paint0_radial_4933_393068"
+                                cx="0"
+                                cy="0"
+                                r="1"
+                                gradientUnits="userSpaceOnUse"
+                                gradientTransform="translate(240 240) rotate(90) scale(240 240)">
+                                <stop />
+                                <stop offset="1" stopOpacity="0" />
+                            </radialGradient>
+                        </defs>
+                    </svg>
+                </div>
 
-            <Form
-              onSubmit={(e) => {
-                e.preventDefault();
-                const data = Object.fromEntries(new FormData(e.currentTarget));
-                console.log("Form data:", data);
-              }}
-              className="mt-8 flex w-full flex-col items-stretch gap-4 md:mt-12 md:max-w-[480px] md:flex-row md:items-start"
-            >
-              <Input
-                isRequired
-                size="md"
-                name="email"
-                type="email"
-                wrapperClassName="py-0.5"
-                placeholder="Enter your email"
-                validate={(value) =>
-                  value.includes("@") || "Please enter a valid email"
-                }
-                hint={
-                  <span>
-                    We care about your data in our{" "}
-                    <a
-                      href="#"
-                      className="rounded-sm underline underline-offset-3 outline-focus-ring focus:outline-2 focus:outline-offset-2"
-                    >
-                      privacy policy
-                    </a>
-                    .
-                  </span>
-                }
-              />
-              <Button type="submit" size="xl">
-                Get started
-              </Button>
-            </Form>
-          </div>
+                <h1 className="max-w-3xl text-center text-display-sm font-semibold text-primary">
+                    Welcome to the beginning of your next beautiful app
+                </h1>
+
+                <p className="mt-2 max-w-xl text-center text-lg text-tertiary">
+                    Get started by either using existing components that came with this starter kit or add new ones:
+                </p>
+
+                <div className="relative mt-6 flex h-10 items-center rounded-lg border border-secondary bg-secondary">
+                    <code className="px-3 font-mono text-secondary">npx untitledui@latest add</code>
+
+                    <hr className="h-10 w-px bg-border-secondary" />
+
+                    <ButtonUtility color="tertiary" size="sm" icon={Copy01} tooltip="Copy" className="mx-1" />
+                </div>
+
+                <div className="mt-6 flex items-center gap-3">
+                    <Button
+                        isExternal
+                        href="https://untitledui-docs.vercel.app/"
+                        color="link-color"
+                        size="lg"
+                        iconLeading={BookOpen01}>
+                        Docs
+                    </Button>
+                    <div className="h-px w-4 bg-brand-solid" />
+                    <Button
+                        isExternal
+                        href="https://untitledui-docs.vercel.app/resources/icons"
+                        color="link-color"
+                        size="lg"
+                        iconLeading={Cube01}>
+                        Icons
+                    </Button>
+                    <div className="h-px w-4 bg-brand-solid" />
+                    <Button
+                        isExternal
+                        href="https://dsc.gg/untitledui"
+                        color="link-color"
+                        size="lg"
+                        iconLeading={HelpCircle}>
+                        Help
+                    </Button>
+                </div>
+            </div>
         </div>
-        <div className="relative mt-16 h-[240px] w-full px-4 md:h-[380px] md:px-8 lg:absolute lg:inset-y-0 lg:right-0 lg:mt-0 lg:h-full lg:w-1/2 lg:px-0">
-          <img
-            className="inset-0 size-full object-cover lg:absolute"
-            src="/marketing/spirals.webp"
-            alt="Spirals"
-          />
-        </div>
-      </section>
-    </Fragment>
-  );
+    );
 };
