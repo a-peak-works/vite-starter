020f77e81de63977c8ac2d4f094ca40da9dc3f4c
xelopsys <juraqulovjamshid7@gmail.com>
2025-05-13 20:39:34 +0500

    fix: button utility is updated

--- src/components/shared/buttons/button-utility.tsx
+++ src/components/shared/buttons/button-utility.tsx
@@ -1,81 +1,70 @@
 "use client";
 
 import type { DetailedHTMLProps, FC, HTMLAttributes, ReactNode } from "react";
-import React, { isValidElement } from "react";
+import { isValidElement } from "react";
+import type { Placement } from "react-aria";
 import type { ButtonProps as AriaButtonProps } from "react-aria-components";
-import { Button as AriaButton } from "react-aria-components";
-import { cx, sortCx } from "@/components/utils/cx";
+import { Button as AriaButton, Link as AriaLink } from "react-aria-components";
+import { cx } from "@/components/utils/cx";
 import { isReactComponent } from "@/components/utils/is-react-component";
 import { Tooltip } from "../tooltip/tooltip";
 
-export const styles = sortCx({
-    common: {
-        root: "group relative inline-flex h-max cursor-pointer items-center justify-center outline-focus-ring transition duration-100 ease-linear focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:text-fg-disabled_subtle",
-        icon: "pointer-events-none shrink-0 text-current transition-inherit-all",
-    },
-
-    sizes: {
-        xs: {
-            root: "rounded-md p-1.5",
-            icon: "size-4",
-        },
-        sm: {
-            root: "rounded-md p-1.5",
-            icon: "size-5",
-        },
-    },
-
-    colors: {
-        secondary: {
-            root: "bg-primary text-fg-quaternary shadow-xs-skeumorphic ring-1 ring-border-primary ring-inset hover:bg-primary_hover hover:text-fg-quaternary_hover disabled:shadow-xs disabled:ring-border-disabled_subtle",
-        },
-        tertiary: {
-            root: "text-fg-quaternary hover:bg-primary_hover hover:text-fg-quaternary_hover",
-        },
-    },
-});
+export const styles = {
+    secondary:
+        "bg-primary text-fg-quaternary shadow-xs-skeumorphic ring-1 ring-border-primary ring-inset hover:bg-primary_hover hover:text-fg-quaternary_hover disabled:shadow-xs disabled:ring-border-disabled_subtle",
+    tertiary: "text-fg-quaternary hover:bg-primary_hover hover:text-fg-quaternary_hover",
+};
 
 export interface ButtonUtilityProps extends DetailedHTMLProps<Omit<HTMLAttributes<HTMLButtonElement>, "color" | "slot">, HTMLButtonElement> {
-    disabled?: boolean;
-    color?: keyof typeof styles.colors;
-    size?: keyof typeof styles.sizes;
+    isDisabled?: boolean;
+    color?: "secondary" | "tertiary";
+    size?: "xs" | "sm";
     icon?: FC<{ className?: string }> | ReactNode;
     slot?: AriaButtonProps["slot"];
     tooltip?: string;
+    tooltipPlacement?: Placement;
 }
 
-const ButtonUtility = ({ size = "sm", color = "secondary", className, icon: Icon, children, onClick, disabled, tooltip, ...rest }: ButtonUtilityProps) => {
-    const Component = "href" in rest ? "a" : AriaButton;
+export const ButtonUtility = ({
+    tooltip,
+    children,
+    className,
+    isDisabled,
+    icon: Icon,
+    size = "sm",
+    color = "secondary",
+    tooltipPlacement = "top",
+    ...otherProps
+}: ButtonUtilityProps) => {
+    const Component = "href" in otherProps ? AriaLink : AriaButton;
 
     const content = (
         <Component
-            isDisabled={disabled}
             type="button"
+            aria-label={tooltip}
+            isDisabled={isDisabled}
             // Remove `any` type assertion after splitting
             // Component into Link and Button.
-            {...(rest as any)}
-            onPress={(event) => {
-                // @ts-expect-error FIX ME
-                rest.onPress?.(event);
-                onClick?.(event as any);
-            }}
+            {...(otherProps as any)}
             className={cx(
-                styles.common.root,
-                styles.sizes[size].root,
-                styles.colors[color].root,
+                "group relative inline-flex h-max cursor-pointer items-center justify-center rounded-md p-1.5 outline-focus-ring transition duration-100 ease-linear focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:text-fg-disabled_subtle",
+                styles[color],
 
-                color === "tertiary" && size === "xs" && "p-2",
+                // Icon styles
+                "*:data-icon:pointer-events-none *:data-icon:shrink-0 *:data-icon:text-current *:data-icon:transition-inherit-all",
+                size === "xs" ? "*:data-icon:size-4" : "*:data-icon:size-5",
+
                 className,
             )}
         >
-            {isReactComponent(Icon) && <Icon data-icon="leading" className={cx(styles.common.icon, styles.sizes[size].icon)} />}
+            {isReactComponent(Icon) && <Icon data-icon />}
             {isValidElement(Icon) && Icon}
         </Component>
     );
 
     if (tooltip) {
         return (
-            <Tooltip title={tooltip} offset={size === "xs" ? 4 : 6}>
+            <Tooltip title={tooltip} placement={tooltipPlacement} isDisabled={isDisabled} offset={size === "xs" ? 4 : 6}>
                 {content}
             </Tooltip>
         );
@@ -83,5 +72,3 @@ const ButtonUtility = ({ size = "sm", color = "secondary", className, icon: Icon
 
     return content;
 };
-
-export default ButtonUtility;
